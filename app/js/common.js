65ea447c64a0531e57bac0bb10ebf9d92f8a40d9
alex <alex_kobilianskiy@bigmir.net>
2020-01-20 18:46:07 +0200

    section contacts complete

--- app/js/common.js
+++ app/js/common.js
@@ -50,6 +50,20 @@ $(document).ready(function(){
     });
 
 
+    var uPhone = $('.user-phone');
+    uPhone.mask("+7 (999) 999-99-99",{autoclear: false});
+
+    uPhone.on('click', function (ele) {
+        var needelem = ele.target || event.srcElement;
+        needelem.setSelectionRange(4,4);
+        needelem.focus();
+    });
+
+    $.validate({
+        form : '.contact-form, .getinfo-form',
+        scrollToTopOnError: false
+    });
+
     //E-mail Ajax Send
     $("form").submit(function() { //Change
         var th = $(this);
